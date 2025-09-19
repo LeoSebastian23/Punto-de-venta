d2e9da61ea1c871c63b6b328c23136e30ecb770c
Leonardo Sebastian Gauto <leosebastiang19@gmail.com>
2025-09-18 21:13:25 -0300

    Carpetas

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace Punto_de_venta
         {
             InitializeComponent();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
